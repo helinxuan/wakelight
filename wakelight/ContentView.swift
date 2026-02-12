a0a1cfb039b5130eda3c80314267df83ea1480a0
helinxuan <1061267396@qq.com>
2026-02-12 22:03:23 +0800

    Initial Commit

---
+++ wakelight/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  wakelight
+//
+//  Created by helinxuan on 2026/2/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ wakelight/wakelightApp.swift
@@ -0,0 +1,17 @@
+//
+//  wakelightApp.swift
+//  wakelight
+//
+//  Created by helinxuan on 2026/2/12.
+//
+
+import SwiftUI
+
+@main
+struct wakelightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
